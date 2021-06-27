1bdc91053e73fb5d14d7342a44df54387fc8bca0
mtarique <mtarique@outlook.com>
2021-06-27 17:42:54 +0530

    Added nodemon and setup.

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/', function(req, res) {
         </head>
         <body>
         <div class="container">
-            <h1 class="display-4 text-center py-1">To-Do App</h1>
+            <h1 class="display-4 text-center py-1">To-Do App!</h1>
             
             <div class="jumbotron p-3 shadow-sm">
             <form action="/create-item" method="POST">
