72400e08044fc33521a2f9a9619ee8cfd75ec881
mtarique <mtarique@outlook.com>
2021-07-10 14:38:42 +0530

    Create New Item Without Page Reload

--- public/browser.js
+++ public/browser.js
@@ -1,3 +1,30 @@
+function itemTemplate(item) {
+    return `
+        <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+            <span class="item-text">${item.text}</span>
+            <div>
+            <button data-id="${item._id}" class="edit-task btn btn-secondary btn-sm mr-1">Edit</button>
+            <button data-id="${item._id}" class="delete-task btn btn-danger btn-sm">Delete</button>
+            </div>
+        </li>
+    `
+}
+
+// Create Feature
+let new_task = document.getElementById('create-field')
+document.getElementById("create-form").addEventListener("submit", function(e) {
+    e.preventDefault()
+
+    axios.post('/create-item', {text: new_task.value}).then(function(response) {
+        document.getElementById("item-list").insertAdjacentHTML("beforeend", itemTemplate(response.data))
+
+        new_task.value = ""
+        new_task.focus()
+    }).catch(function() {
+        console.log("Please try again later.")
+    }) 
+})
+
 document.addEventListener("click", function(e) {
     // Delete task 
     if(e.target.classList.contains("delete-task")) { 
