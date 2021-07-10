038d27a82aee5474986a96a23f7f41522f79129c
mtarique <mtarique@outlook.com>
2021-07-10 17:55:05 +0530

    Removed config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-var config = {}; 
-
-config.db = {}; 
-
-config.db_connection = 'mongodb+srv://todoAppDBUser:todo-app@7@cluster0.xc2au.mongodb.net/TodoAppDB?retryWrites=true&w=majority'; 
-
-module.exports = config; 
