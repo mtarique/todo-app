4d11a72db5b5b53d6bc7ec9dbdd7b56e4272a03c
mtarique <mtarique@outlook.com>
2021-07-10 17:53:06 +0530

    Added config file.

---
+++ config.js
@@ -0,0 +1,7 @@
+var config = {}; 
+
+config.db = {}; 
+
+config.db_connection = 'mongodb+srv://todoAppDBUser:todo-app@7@cluster0.xc2au.mongodb.net/TodoAppDB?retryWrites=true&w=majority'; 
+
+module.exports = config; 
